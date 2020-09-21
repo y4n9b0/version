85873c84ff0f2e7e601783b95775d34e3c541d7b
yangbo <yangbo243@jd.com>
2020-09-21 09:58:24 +0800

    feat: 插件遍历并打印版本号，便于调试

--- plugin/src/main/java/com/step2hell/version/VersionPlugin.kt
+++ plugin/src/main/java/com/step2hell/version/VersionPlugin.kt
@@ -2,10 +2,19 @@ package com.step2hell.version
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import kotlin.reflect.full.memberProperties
 
 class VersionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        println("===========================> beginning of plugin 'VersionPlugin'")
+        println("\n====> Print Versions:")
+        for (prop in Versions::class.memberProperties) {
+            println("${prop.name} = ${prop.get(Versions)}")
+        }
+
+        println("\n====> Print Dependencies:")
+        for (prop in Dependencies::class.memberProperties) {
+            println("${prop.name} = ${prop.get(Dependencies)}")
+        }
     }
 
     companion object {}
